ebc39dd6fdc4e40de2a27b4c3a40044da2c2c884
yrathinasamy <ryuvarajeee@gmail.com>
2022-01-13 16:02:49 -0500

    Added New Tests
    Added Negative tests

---
+++ src/test/java/com/yuvaraj/pagination_helper/PaginationHelperTestNotHappy.java
@@ -0,0 +1,70 @@
+package com.yuvaraj.pagination_helper;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class PaginationHelperTestNotHappy {
+	PaginationHelper<String> helper;
+
+	@Test(expected = Error.class)
+	@DisplayName("PaginationHelper class instantiation Negative case#1")
+	public void setUp1() {
+		String[] strList = new String[10];
+		List<String> list = new ArrayList<>(Arrays.asList(strList));
+		helper = new PaginationHelper<>(list);
+	}
+
+	@Test(expected = Error.class)
+	@DisplayName("PaginationHelper class instantiation Negative case#2")
+	public void setUp2() {
+		helper = new PaginationHelper<>(4);
+	}
+
+	@Test(expected = Error.class)
+	@DisplayName("PaginationHelper class instantiation Negative case#3")
+	public void setUp3() {
+		helper = new PaginationHelper<>();
+	}
+
+	@Test
+	@DisplayName("PaginationHelper class instantiation size zero list input")
+	public void setUp4() {
+		String[] strList = new String[] {};
+		List<String> list = new ArrayList<>(Arrays.asList(strList));
+		this.helper = new PaginationHelper<>(list, 4);
+		assertEquals(0, helper.itemCount());
+		assertEquals(0, helper.pageCount());
+	}
+	
+	@Test(expected = NullPointerException.class)
+	@DisplayName("PaginationHelper class Not instantiated")
+	public void setUp5() {
+		helper.itemCount();
+		helper.pageCount();
+	}
+	
+	@Test(expected = Error.class)
+	@DisplayName("PageItemCount Method Negative case")
+	public void testPageItemCountNegative() {
+		String[] strList = new String[] { "a", "b", "c", "d", "e", "f" };
+		List<String> list = new ArrayList<>(Arrays.asList(strList));
+		this.helper = new PaginationHelper<>(list, 4);
+		helper.pageItemCount("a");
+		helper.pageIndex();
+	}
+	
+	@Test(expected = Error.class)
+	@DisplayName("PageIndex Method Negative case")
+	public void testPageIndexNegative() {
+		String[] strList = new String[] { "a", "b", "c", "d", "e", "f" };
+		List<String> list = new ArrayList<>(Arrays.asList(strList));
+		this.helper = new PaginationHelper<>(list, 4);
+		helper.pageIndex("a");
+		helper.pageIndex();
+	}
+}
